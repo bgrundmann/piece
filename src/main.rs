2eb8c181b3c358b16bbd45dd74091fadacf721b8
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2015-05-25 21:10:03 +0100

    Append

--- src/main.rs
+++ src/main.rs
@@ -297,6 +297,15 @@ impl Text {
         self.invariant()
     } 
 
+    /// Append bytes at end.
+    pub fn append(&mut self, bytes: &[u8]) {
+        if bytes.len() == 0 {
+            return;
+        } 
+        let off = self.len() as u32;
+        self.insert(off, bytes);
+    } 
+
     /// Insert bytes at offset.
     pub fn insert(&mut self, off:u32, bytes: &[u8]) {
         if bytes.len() == 0 {
@@ -478,135 +487,14 @@ mod tests {
         } 
     } 
 }
-/*
 
-
-
-impl Text {
-    pub fn from_str(s: &str) -> Text {
-        let mut buffer = AppendOnlyBuffer::new();
-        let span       = buffer.append(s);
-        let piece_data = PieceData::new(span);
-        Text {
-          buffer: buffer,
-          pieces: vec![piece_data],
-          first: Piece(0)
-        } 
-    } 
-
-    fn get_mut_piece(&mut self, Piece(p1): Piece) -> &mut PieceData {
-        &mut self.pieces[p1 as usize]
-    } 
-
-    fn get_piece(&self, Piece(p1): Piece) -> &PieceData {
-        &self.pieces[p1 as usize]
-    } 
-
-    fn iter_pieces(&self) -> Pieces {
-        Pieces {
-            text: self,
-            curr: Some(self.first),
-            off:  0,
-        } 
-    } 
-
-    pub fn to_string(&self) -> String {
-        let mut result = String::new();
-        let spans = self.iter_pieces()
-            .map(|(_, piece)| self.get_piece(piece).span);
-        for span in spans {
-            result.push_str(self.buffer.get(span));
-        } 
-        result
-    } 
-
-    fn last_piece(&self) -> (u32, Piece) {
-        let mut off = 0;
-        let mut piece = self.first;
-        for (o, p) in self.iter_pieces() {
-            off = o;
-            piece = p;
-        } 
-        (off, piece)
-    } 
-
-
-    fn link(&mut self, p1: Piece, p2: Piece) {
-        self.get_mut_piece(p1).next = Some(p2);
-        self.get_mut_piece(p2).prev = Some(p1);
-    } 
-
-    pub fn append(&mut self, s: &str) {
-        if s.len() > 0 {
-            let (_, old_last_piece) = self.last_piece();
-            let span       = self.buffer.append(s);
-            let piece_data = PieceData::new(span);
-            self.pieces.push(piece_data);
-            let new_last_piece = Piece( (self.pieces.len() - 1) as u32);
-            self.link(old_last_piece, new_last_piece)
-        } 
-    } 
-
-    pub fn delete(&mut self, span:Span) {
-        //  0123  456  789
-        // [XXYY][YYY][YXX]
-        // 
-        // delete [2-8)
-        //
-        match (self.piece_containing(span.off1), self.piece_containing(span.off2)) {
-            None, None    => panic!("invalid span to delete"),
-            None, Some(_) => panic!("invalid span to delete"),
-            Some(_), None => panic!("invalid span to delete"),
-            Some ((start1, piece1)), Some ((start2, piece2)) => {
-                if (piece1 = piece2) {
-                    // special case deletion in one piece
-
-                } 
-            } 
-        } 
-    } 
-} 
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn from_str_test() {
-        let text = Text::from_str("Hello");
-        assert_eq!(text.to_string(), "Hello");
-    } 
-
-    #[test]
-    fn append_test() {
-        let mut text = Text::from_str("Hello");
-        text.append(" ");
-        text.append("World");
-        assert_eq!(text.to_string(), "Hello World");
-    } 
-
-    #[test]
-    fn iter_offset_test() {
-        let mut text = Text::from_str("Hello");
-        text.append(" ");
-        text.append("World");
-        let expected = vec![0, 5, 6];
-        let actual: Vec<_> = text.iter_pieces().map(|(o,_)| o).collect();
-        assert_eq!(actual, expected);
-    } 
-} 
-
+#[cfg(not(test))]
 fn main() {
     env_logger::init().unwrap();
     info!("starting up");
 
-    let mut text = Text::from_str("Hello");
-    text.append(" ");
-    text.append("World!");
-    println!("{:?}", text);
-    for (off, piece) in text.iter_pieces() {
-        println!("{}: {:?}", off, piece);
-    } 
-    println!("{}", text.to_string());
+    let mut text = Text::new();
+    text.append("Hello".as_bytes());
+    text.append(" ".as_bytes());
+    text.append("World!".as_bytes());
 }
-*/
